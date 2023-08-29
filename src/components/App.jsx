a57d1c6fafa6bb40b7a3304a5d66b088a78548d0
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-08-28 21:52:15 -0600

    deconstruction, useState, increase and decrease functions hooked to the buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,22 @@ import "../styles.css";
 
 //UseState updates the parts that needs to be changed
 function App() {
-  const state = useState(2);
-  console.log(state[0]);
+  const [count, setCount] = useState(0);
+
+  //const [red, green, blue] = [116, 185, 255]; //Deconstruction arrays
 
   function increase() {
-    //count++;
+    setCount(count + 1);
   }
 
+  function decrease() {
+    setCount(count - 1);
+  }
   return (
     <div className="container">
-      <h1>{state[0]}</h1>
+      <h1>{count}</h1>
       <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
     </div>
   );
 }
