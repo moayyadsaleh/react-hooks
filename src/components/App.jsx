8e82bb76a6c5431f6f529b7150a05bef57ad5efb
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-08-28 21:33:23 -0600

    Use useState Function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react"; //Import useState
 import "../styles.css";
 
+//UseState updates the parts that needs to be changed
 function App() {
-  return <div />;
+  const state = useState(2);
+  console.log(state[0]);
+
+  function increase() {
+    //count++;
+  }
+
+  return (
+    <div className="container">
+      <h1>{state[0]}</h1>
+      <button onClick={increase}>+</button>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./components/App";
-import reportWebVitals from "./reportWebVitals";
-
-var count = 0;
-function increase() {
-  count++;
-  root.render(
-    <React.StrictMode>
-      <App />
-      <div className="container">
-        <h1>{count}</h1>
-        <button onClick={increase}>+</button>
-      </div>
-    </React.StrictMode>
-  );
-}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <div className="container">
-      <h1>{count}</h1>
-      <button onClick={increase}>+</button>
-    </div>
   </React.StrictMode>
 );
-
-reportWebVitals();
