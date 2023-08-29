ba12d96a7d9b9b4254050c216998c07424ace693
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-08-28 20:55:10 -0600

    update UI by rendering the react dom in the function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+var count = 0;
+function increase() {
+  count++;
+  root.render(
+    <React.StrictMode>
+      <App />
+      <div className="container">
+        <h1>{count}</h1>
+        <button onClick={increase}>+</button>
+      </div>
+    </React.StrictMode>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+    </div>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
